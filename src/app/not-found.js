e44ec4b259a053a88cdf828ec185f3f119addbfb
MuhammadMaruf11 <muhammad11maruf@gmail.com>
2024-04-21 19:48:52 +0600

    complete web without rwd

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -10,8 +10,8 @@ export default function NotFound() {
     return (
         <main>
             <SubBannerComp mainTitle={mainTitle} />
-            <div className='flex flex-col  items-center justify-center pb-20'>
-                <img width={1080} height={608} src='/img/bg/404notfoundGrandeurae-01.webp' alt='error' />
+            <div className='flex flex-col  items-center justify-center py-8'>
+                <img width={480} src='/img/bg/404notfoundGrandeurae-01.webp' alt='error' />
                 <Link className="px-5 py-3 text-theme transition-all duration-300 ease-linear border hover:text-theme-2 hover:border-theme-2  border-theme" href="/">Return Home</Link>
             </div>
         </main>
