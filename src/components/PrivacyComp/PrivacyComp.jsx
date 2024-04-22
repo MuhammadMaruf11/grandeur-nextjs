ca937d690df031c14aa1d73e663a1175ed2f036a
MuhammadMaruf11 <muhammad11maruf@gmail.com>
2024-04-22 12:11:21 +0600

    privacy page and terms page

--- src/components/PrivacyComp/PrivacyComp.jsx
+++ src/components/PrivacyComp/PrivacyComp.jsx
@@ -1,25 +1,37 @@
+/* eslint-disable react/no-unescaped-entities */
 
 const PrivacyComp = () => {
     return (
-        <section className="py-20 bg-[url('/img/bg/BGLine-01.webp')]">
-            <div className="max-w-screen-xl mx-auto bg-indigo-300 bg-opacity-30 backdrop-blur-sm border-2 border-indigo-300  px-7 rounded-md py-14">
-                <h2 className="text-3xl font-bold mb-4 text-center text-theme">Privacy Policy</h2>
-                <p className="text-justify">
-                    At Grandeur Technologies LLC, accessible from yourwebsite.com, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Grandeur Technologies LLC and how we use it.
-                    <br /><br />
-                    If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.
-                    <br /><br />
-                    This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Grandeur Technologies LLC. This policy is not applicable to any information collected offline or via channels other than this website.
-                    <br /><br />
-                    Consent: By using our website, you hereby consent to our Privacy Policy and agree to its terms.
-                    <br /><br />
-                    Information we collect: The personal information that you are asked to provide, and the reasons why you are asked to provide it, will be made clear to you at the point we ask you to provide your personal information.
-                    <br /><br />
-                    If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide.
+        <section className="bg-[url('/img/bg/BGLine-01.webp')] bg-cover bg-no-repeat bg-top py-20">
+            <div className="max-w-screen-lg mx-auto bg-indigo-300 bg-opacity-30 backdrop-blur-sm border-2 border-indigo-300  px-7 rounded-md py-14">
+                <h2 className="text-3xl font-bold mb-4 text-center text-theme">Our Commitment to Protecting Your Privacy</h2>
+                <p className="text-justify mb-4">
+                    Our Privacy Policy explains how we collect, use, and protect your personal information when you use our website or services. We are committed to safeguarding your privacy and ensuring the security of your personal data. This policy outlines our data collection practices, the purposes for which we use your information, your rights regarding your data, and how we protect your privacy. Your privacy matters to us, and we are dedicated to being transparent about our data practices.
                 </p>
+                <ul className=" list-[disclosure-closed] marker:text-[#f37022] list-inside space-y-2 text-justify">
+                    <li><strong>Introduction: </strong>Briefly explain the purpose of the Privacy Policy and its importance for users.</li>
+                    <li><strong>Types of Information Collected: </strong>Detail the types of personal information collected, such as name, email address, phone number, IP address, etc.</li>
+                    <li><strong>Methods of Collection: </strong>Explain how the website collects user information, including cookies, forms, registration, etc.</li>
+                    <li><strong>Purposes of Data Collection: </strong>Describe the purposes for which user data is collected, such as providing services, improving user experience, marketing, etc.</li>
+                    <li><strong>Data Usage and Sharing: </strong>Outline how user data is used and whether it is shared with third parties. Include information about data retention periods and any data processing agreements in place.</li>
+                    <li><strong>Security Measures: </strong>Explain the security measures implemented to protect user data from unauthorized access, disclosure, alteration, or destruction.</li>
+                    <li><strong>User Rights: </strong>Inform users about their rights regarding their personal data, including the right to access, rectify, delete, or restrict processing of their data.</li>
+                    <li><strong>Updates to Privacy Policy: </strong>State that the Privacy Policy may be updated periodically and provide a date of the last update.</li>
+                    <li><strong>Contact Information: </strong>Provide contact details for users to reach out with any questions or concerns regarding the Privacy Policy.</li>
+                    <li><strong>Data Retention: </strong>Specify the period for which user data will be retained and the criteria used to determine the retention period.</li>
+                    <li><strong>Cookies and Tracking Technologies: </strong>Explain the use of cookies and other tracking technologies on the website, including the types of cookies used, their purpose, and how users can manage their cookie preferences.</li>
+                    <li><strong>Third-Party Links: </strong>Clarify that the Privacy Policy applies only to the website and that users should review the privacy policies of any third-party websites linked from the website.</li>
+                    <li><strong>Children's Privacy: </strong>Address how the website handles the privacy of children under the age of 13, in compliance with the Children's Online Privacy Protection Act (COPPA) if applicable.</li>
+                    <li><strong>International Data Transfers: </strong>Disclose if user data may be transferred to countries outside the user's country of residence and the safeguards in place to protect the data during such transfers.</li>
+                    <li><strong>Data Subject Rights: </strong>Detail the rights users have regarding their personal data, such as the right to access, rectify, delete, or object to the processing of their data.</li>
+                    <li><strong>Data Breach Notification: </strong>Outline procedures for notifying users in the event of a data breach, including the timeframe for notification and the information provided to users.</li>
+                    <li><strong>California Privacy Rights: </strong>If applicable, provide information on users' rights under the California Consumer Privacy Act (CCPA), including the right to opt-out of the sale of personal information.</li>
+                    <li><strong>European Union (EU) Data Protection: </strong>If applicable, explain how the website complies with the General Data Protection Regulation (GDPR) requirements for data protection and user privacy.</li>
+                    <li><strong>Contact Information for Data Protection Officer (DPO): </strong>If required by law, provide contact details for the Data Protection Officer responsible for overseeing data protection compliance.</li>
+                </ul>
             </div>
         </section>
-       
+
     );
 };
 
--- src/components/TermsComp/TermsComp.jsx
+++ src/components/TermsComp/TermsComp.jsx
@@ -2,18 +2,34 @@
 
 const TermsComp = () => {
     return (
-        <section className="py-20  bg-[url('/img/bg/BGLine-01.webp')]">
-            <div className="max-w-screen-xl mx-auto  bg-indigo-300 bg-opacity-30 backdrop-blur-sm border-2 border-indigo-300 px-7 rounded-md py-14">
-                <h2 className="text-3xl font-bold mb-4 text-center text-theme">Terms & Conditions</h2>
-                <p className="text-justify">
-                    Welcome to Grandeur Technologies LLC. These terms and conditions outline the rules and regulations for the use of Grandeur Technologies LLC's Website, located at yourwebsite.com.
-                    <br /><br />
-                    By accessing this website, we assume you accept these terms and conditions. Do not continue to use Grandeur Technologies LLC if you do not agree to take all of the terms and conditions stated on this page.
-                    <br /><br />
-                    The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company’s terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.
-                    <br /><br />
-                    These Terms and Conditions have been generated with the help of the Terms And Conditions Template and the Privacy Policy Generator.
+        <section className="bg-[url('/img/bg/BGLine-01.webp')] bg-cover bg-no-repeat bg-top py-20">
+            <div className="max-w-screen-lg mx-auto  bg-indigo-300 bg-opacity-30 backdrop-blur-sm border-2 border-indigo-300 px-7 rounded-md py-14">
+                <h2 className="text-3xl font-bold mb-4 text-center text-theme">Your Agreement to Use Our Services</h2>
+                <p className="text-justify mb-4">
+                    Our Terms & Conditions outline the rules and guidelines governing the use of our website and services. By accessing or using our website, you agree to abide by these terms, which cover important aspects such as user responsibilities, website usage rights, disclaimers, limitations of liability, and more. Please read these terms carefully before using our website.
                 </p>
+                <ul className=" list-[disclosure-closed] marker:text-[#f37022] list-inside space-y-2 text-justify">
+                    <li><strong>Introduction: </strong>Provide an overview of the Terms & Conditions and its applicability to users accessing the website or using its services.</li>
+                    <li><strong>User Responsibilities: </strong>Outline the responsibilities of users when accessing the website or using its services, including compliance with laws, respect for intellectual property rights, etc.</li>
+                    <li><strong>Website Usage Rights: </strong>Detail the rights granted to users for accessing and using the website or its services, including non-exclusive, non-transferable, limited rights.</li>
+                    <li><strong>Disclaimers: </strong>Include disclaimers regarding the accuracy, completeness, reliability of information on the website, and disclaimer of warranties.</li>
+                    <li><strong>Limitations of Liability:</strong>Specify limitations of liability for the ISP website and its owners for any damages arising from the use or inability to use the website or its services.</li>
+                    <li><strong>Intellectual Property Rights: </strong>Assert intellectual property rights over website content, trademarks, logos, etc., and prohibit unauthorized use or reproduction.</li>
+                    <li><strong>User Conduct: </strong>Specify prohibited activities, such as hacking, spamming, uploading malicious content, etc., and consequences for violating terms.</li>
+                    <li><strong>Termination Clause: </strong>State conditions under which the website may terminate or suspend user access to the website or its services.</li>
+                    <li><strong>Governing Law: </strong>Indicate the governing law and jurisdiction for resolving disputes related to the Terms & Conditions.</li>
+                    <li><strong>Updates to Terms & Conditions: </strong>Mention that the Terms & Conditions may be updated periodically and provide a date of the last update.</li>
+                    <li><strong>Contact Information: </strong>Provide contact details for users to reach out with any questions or concerns regarding the Terms & Conditions.</li>
+                    <li><strong>User Account: </strong>If applicable, describe the process for creating a user account on the website, including account registration, username, password, and account security measures.</li>
+                    <li><strong>Payment Terms: </strong>Specify the terms of payment for any products or services offered on the website, including payment methods accepted, billing cycles, and refund policies.</li>
+                    <li><strong>Subscription Services:</strong>If applicable, outline the terms and conditions for subscription-based services, including subscription fees, renewal terms, and cancellation policies.</li>
+                    <li><strong>User-Generated Content: </strong>Address the rights and responsibilities of users regarding content they upload, post, or submit to the website, including ownership, licensing, and moderation policies.</li>
+                    <li><strong>Indemnification: </strong>Explain that users agree to indemnify and hold harmless the website and its owners from any claims, damages, liabilities, costs, or expenses arising from their use of the website or violation of the Terms & Conditions.</li>
+                    <li><strong>Dispute Resolution: </strong>Specify procedures for resolving disputes between users and the website, such as arbitration, mediation, or litigation in a specified jurisdiction.</li>
+                    <li><strong>Force Majeure: </strong>Include a force majeure clause to exempt the website from liability for failure to perform obligations due to unforeseeable circumstances beyond its control, such as natural disasters or acts of war.</li>
+                    <li><strong>Severability: </strong>State that if any provision of the Terms & Conditions is found to be invalid or unenforceable, the remaining provisions will remain in full force and effect.</li>
+                    <li><strong>Entire Agreement: </strong>Clarify that the Terms & Conditions constitute the entire agreement between users and the website, superseding any prior agreements or understandings.</li>
+                </ul>
             </div>
         </section>
     );
